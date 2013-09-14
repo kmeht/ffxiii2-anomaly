6a67408eb557e90c736bb9325d071973e2578e47
Kunal Mehta <k.mehta@berkeley.edu>
2013-09-14 01:44:12 -0700

    Comment cleanup.

--- anomaly.rb
+++ anomaly.rb
@@ -19,13 +19,12 @@
   
   04/19/2012 - original
   04/20/2012 - refactoring
+  09/13/2013 - code cleanup
 
   Use of this source code is governed by a BSD license found in README.md.
 
 =end
 
-
-# GLOBAL STATE
 
 # The clock is represented as an array of pairs of the form [value, active?].
 $clock = ARGV[0].split(',').map{|val| [val.to_i, true]}
@@ -37,49 +36,45 @@ $hands = [0, 0]
 $moves = []
 
 
-# HELPER FUNCTIONS
-
-# Returns the value of a node.
+# Return the value of a node.
 def value(index)
   $clock[index][0]
 end
 
-# Checks if a node is empty.
+# Check if a node is empty.
 def empty?(index)
   not $clock[index][1]
 end
 
-# Checks if every node is empty.
+# Check if every node is empty.
 def win?
   $clock.find_all{|node| node[1]}.empty?
 end
 
-# Checks if both clock hands point to empty nodes.
+# Check if both clock hands point to empty nodes.
 def lose?
   empty?($hands[0]) and empty?($hands[1])
 end
 
-# Updates the clock hands based on a move.
+# Update the clock hands based on a move.
 def update_hands(move)
   $hands = [-value(move), value(move)].map{|h| (h + move) % $clock.length} rescue [0,0] # in case the hands revert to initial state
 end
 
-# Executes the move and updates state.
+# Execute the move and update state.
 def execute(move)
   $moves << move
   $clock[move][1] = false
   update_hands(move)
 end
 
-# Reverts state to before the last move.
+# Revert state to before the last move.
 def revert
   $clock[$moves.pop][1] = true
   update_hands($moves[-1])
 end
 
-
-
-# The main function of the script.
+# Search through the space of possible moves starting at 'move'.
 def do_move(move)
   return if empty?(move)
   execute(move)
@@ -95,7 +90,7 @@ def do_move(move)
   end
 end
 
+
 # Try the chain of moves from each starting point.
 (0..($clock.length - 1)).each{|start| do_move(start)}
 puts 'Unable to find a solution.'
-
